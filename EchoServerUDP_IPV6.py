1699c2ea0c2b0229a2d327144cc3ff7021d6fc4a
Ferenc Gemesi <ferenc.gemesi@gmail.com>
2020-07-21 12:13:03 +0200

    finish7

--- EchoServerUDP_IPV6.py
+++ EchoServerUDP_IPV6.py
@@ -41,7 +41,7 @@ def myDate():
 
 #*********************************************************************************
 
-print('ARGV      :', sys.argv[1:])
+# print('ARGV      :', sys.argv[1:])
 
 try:
     options, remainder = getopt.getopt(
@@ -57,8 +57,8 @@ except getopt.GetoptError as err:
     print('ERROR:', err)
     sys.exit(1)
 
-print('OPTIONS   :', options)
-print('REMAINING :', remainder)
+# print('OPTIONS   :', options)
+# print('REMAINING :', remainder)
 
 msg_indent = (" ".rjust(len(os.path.split(sys.argv[0])[1]) + 1))
 
@@ -75,30 +75,28 @@ Version: {ver}
 
 docstring = '''\
 Documentation: {ver}
-Goal.
-IPv6 address discovery of host in communication.
-
-Realization.
+### Goal.
+IPv6 address discovery of host in IP network communication.
+### Realization.
 Client-server architecture. Client(IPv6EchoClient) send request to server(Pv6Echo), the server sends her reply to client, what was the ip adders which client used.
 My solution the IPv6 Echo.py, IPv6EchoClient.py pair, written in Python programming language. Using UDP protocoll.
-
-Parametrization.
-{help}
-Usage instruction tricks.
+### Parametrization.
+```{help}```
+### Usage instruction tricks.
 If the server and client in the same host, we can detect the client actual Global Unicast IPv6  Address(ipv6 or temporary ipv6...).
 (The example is in Linux-Ubuntu 20.04 LTS environment)
 The method:
 iptables rule
-ip6tables -A OUTPUT -t nat -p udp --dport 5001 -j DNAT --to [::1]:5000
+```ip6tables -A OUTPUT -t nat -p udp --dport 5001 -j DNAT --to [::1]:5000```
 (forward from port 5001 to [::1]:5000 ipaddress, port in OUTPUT chain, nat table)
-# ip6tables -t nat -v -L -n --line-number 
+```\# ip6tables -t nat -v -L -n --line-number```
 (list nat table rules)
-# ip6tables -t nat -D OUTPUT 1
+```\# ip6tables -t nat -D OUTPUT 1```
 (delete rule #1 from OUTPUT chain nat table) 
 Starting the server
-EchoServerUDP_IPV6.py -p 5000
+```EchoServerUDP_IPV6.py -p 5000```
 Starting the client
-EchoServerUDP_IPV6Client.py -i 2a00:1450:400d:805::200e -p 5001 # google.com IPv6 address
+```EchoServerUDP_IPV6Client.py -i 2a00:1450:400d:805::200e -p 5001 # google.com IPv6 address```
 
 
             '''.format(ver=version, help=usagestring)
